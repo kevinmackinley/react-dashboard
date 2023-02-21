2cf4aa00dc9b45f0d126d9b3563a4cbe699a046b
Kevin Mackinley <kev.mackinley@gmail.com>
2023-02-21 15:06:26 +1100

    added topbar

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,25 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import { LightModeOutlined } from "@mui/icons-material";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsModeOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonModeOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <IconButton sx={{ display: "flex" }}></IconButton>
+    </Box>
+  );
+};
+
+export default Topbar;
